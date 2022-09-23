abaf75b4f04c3557df188029224862f49565f521
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-09-22 20:35:09 -0700

    0x01-python-if_else_loops_functions

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    i = 1
+            length = len(sys.argv) - 1
+
+                if length == 0:
+                    print("{:d} arguments.".format(length))
+                elif length == 1:
+                    print("{:d} argument:".format(length))
+                else:
+                    print("{:d} arguments:".format(length))
+                                                                while i <= length:
+                                                                    print("{:d}: {:s}".format(i, sys.argv[i]))
+                                                                                    i += 1
