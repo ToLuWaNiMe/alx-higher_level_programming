3987d46f423475a8da546ebc40528bcce2c8cac5
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-09-22 20:50:48 -0700

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+            list = dir(hidden_4)
+                for s in list:
+                    if s[0] != '_':
+                        print("{}".format(s))
+
