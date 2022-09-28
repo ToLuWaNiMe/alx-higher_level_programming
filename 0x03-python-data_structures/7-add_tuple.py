b19fd291ab8f9a7f0ed5a5e65a148da5ea76d0a4
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-09-28 12:03:31 -0700

    ririririr

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,6 +14,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = (0, 0)
     elif lenB < 2:
         tuple_b = (tuple_b[0], 0)
-          
+
     res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return res
